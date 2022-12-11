68c14e242d3869913eebc0d8ead306f1960073c0
Janda Baper <78964491+janda-baper@users.noreply.github.com>
2022-12-12 00:22:23 +0700

    Kadals

--- menu/auto-pointing.sh
+++ menu/auto-pointing.sh
@@ -20,11 +20,11 @@ echo "$SUB_DOMAIN" >> /etc/v2ray/domain
 #
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 subsl=$(</dev/urandom tr -dc a-z0-9 | head -c5)
-DOMAIN=mantapxsl.my.id
-SUB_DOMAIN=onichan-${sub}.mantapxsl.my.id
-NS_DOMAIN=zerosl-${sub}.mantapxsl.my.id
-CF_ID=slinfinity69@gmail.com
-CF_KEY=dd2c5e0313f122b3c1833471d469b1025f492
+DOMAIN=ipaang.com
+SUB_DOMAIN=onichan-${sub}.ipaang.com
+NS_DOMAIN=zerosl-${sub}.ipaang.com
+CF_ID=jandabaper09@gmail.com
+CF_KEY=3b05a421b699a05a6ceb09c313d318f585b15
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
 echo "Updating DNS for ${SUB_DOMAIN}..."
